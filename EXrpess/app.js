6d79ea731dd015dd288de694e6f158018d39cfe5
Rakshit sharma <150055691+Rakshit-9999@users.noreply.github.com>
2026-05-11 14:43:35 +0530

    Express Js start

---
+++ EXrpess/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+ const http = require('http').createServer(app);
+ app.use((req ,res ,  next)=>{
+    console.log("Came in first middleware", req.url, req.method);
+    next();
+    
+ });
+ app.use((req ,res ,  next)=>{
+    console.log("Came in Second middleware", req.url, req.method);
+    next();
+ });
+
+const port = 3001;
+http.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}
+);
